ea57b876ea9dc31abb92b786e537ca134de0ae6a
Rohit <rwagh0450@gmail.com>
2026-02-13 16:23:30 +0530

    admin-dashboard-resolve-&-navbar-short-changes

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,18 +1,34 @@
 
 
-
 "use client";
 
+import Navbar from "@/components/navbar";
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // ❌ NO auth logic here
-  // ❌ NO router
-  // ❌ NO localStorage
-  // ❌ NO role check
-
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      {children}
+    </div>
+  );
 }
 
+
+
+// "use client";
+
+// export default function AdminLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   // ❌ NO auth logic here
+//   // ❌ NO router
+//   // ❌ NO localStorage
+//   // ❌ NO role check
+
+//   return <>{children}</>;
+// }
